b776509b9fb8bb9336e6ee053c9bb1c5665c0e0d
Nikita <101108890+nvadeveloper@users.noreply.github.com>
2024-02-14 23:28:10 +0300

    refactor Header and Navbar components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,21 @@ import Link from 'next/link';
 export const Header = () => {
     return (
         <header>
-            <div className="p-4 bg-black text-white flex">
-                <div className='pr-6'>
+            <div className="p-4 bg-black text-white flex justify-between">
+                <div className="pr-6">
                     <Link href="/" className="text-5xl font-bold">
                         Finversia
                     </Link>
                     <p className="text-xs mt-3">Курсы валют: USD 100, EUR 120</p>
-                </div>
-                <div className="text-sm flex flex-col gap-y-2 mx-20">
-                    <p>Немецкие вложения в Китай достигли исторического максимума</p>
-                    <p>
-                        Thyssenkrupp снизила прогнозы по продажам и чистой прибыли из-за проблем в
-                        стале...
-                    </p>
-                    <p>
-                        Sony снизила прогноз продаж по PS5 и готовится к IPO своего финансового
-                        подразде...
-                    </p>
+                    <ul className="flex mt-12 justify-between border-b border-black relative space-x-2">
+                        <li className="cursor-pointer hover:text-gray-400">НОВОСТИ</li>
+                        <li className="cursor-pointer hover:text-gray-400">ПУБЛИКАЦИИ</li>
+                        <li className="cursor-pointer hover:text-gray-400">ИНВЕСТКЛУБ</li>
+                        <li className="cursor-pointer hover:text-gray-400">КАЛЕНДАРЬ</li>
+                        <li className="cursor-pointer hover:text-gray-400">КОТИРОВКИ</li>
+                        <li className="cursor-pointer hover:text-gray-400">СЕРВИСЫ</li>
+                        <li className="cursor-pointer hover:text-gray-400">TV</li>
+                    </ul>
                 </div>
 
                 <div className="flex">
@@ -27,7 +25,6 @@ export const Header = () => {
                         width="300"
                         src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=v_T_vypFOzLYNT6B"
                         title="YouTube video player"
-                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowFullScreen></iframe>
                 </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,13 @@
 export const Navbar = () => {
     return (
         <nav>
-            <ul className="flex justify-between border-b border-black px-4 relative">
-                <li className="cursor-pointer">Экономика и общество</li>
-                <li className="cursor-pointer">Фондовый рынок</li>
-                <li className="cursor-pointer">Валютный рынок</li>
-                <li className="cursor-pointer">Другие рынки</li>
-                <li className="cursor-pointer">Финансовая инфраструктура</li>
-                <li className="cursor-pointer">Больше</li>
-                <div className="absolute top-0 left-0 w-full bg-white">
-                    <ul>
-                        <li>Экономика</li>
-                        <li>Экономика</li>
-                        <li>Экономика</li>
-                        <li>Экономика</li>
-                        <li>Экономика</li>
-                    </ul>
-                </div>
+            <ul className="px-4 flex justify-between border-b border-black relative">
+                <li className="cursor-pointer hover:text-gray-500">Экономика и общество</li>
+                <li className="cursor-pointer hover:text-gray-500">Фондовый рынок</li>
+                <li className="cursor-pointer hover:text-gray-500">Валютный рынок</li>
+                <li className="cursor-pointer hover:text-gray-500">Другие рынки</li>
+                <li className="cursor-pointer hover:text-gray-500">Финансовая инфраструктура</li>
+                <li className="cursor-pointer hover:text-gray-500">Больше</li>
             </ul>
         </nav>
     );
