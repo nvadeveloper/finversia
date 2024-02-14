74f8bafcf1f652575c9890006a6f6d7c2b9ecda6
Nikita <101108890+nvadeveloper@users.noreply.github.com>
2024-02-14 22:04:47 +0300

    add submenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,26 @@ import Link from 'next/link';
 export const Header = () => {
     return (
         <header>
-            <div className="p-4 bg-black text-white flex justify-between">
-                <div>
-                    <Link href="/" className="text-3xl font-bold">
+            <div className="p-4 bg-black text-white flex">
+                <div className='pr-6'>
+                    <Link href="/" className="text-5xl font-bold">
                         Finversia
                     </Link>
-                    <p className="text-sm mt-3">Курсы валют: USD 100, EUR 120</p>
+                    <p className="text-xs mt-3">Курсы валют: USD 100, EUR 120</p>
                 </div>
-                <div>
+                <div className="text-sm flex flex-col gap-y-2 mx-20">
+                    <p>Немецкие вложения в Китай достигли исторического максимума</p>
+                    <p>
+                        Thyssenkrupp снизила прогнозы по продажам и чистой прибыли из-за проблем в
+                        стале...
+                    </p>
+                    <p>
+                        Sony снизила прогноз продаж по PS5 и готовится к IPO своего финансового
+                        подразде...
+                    </p>
+                </div>
+
+                <div className="flex">
                     <iframe
                         width="300"
                         src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=v_T_vypFOzLYNT6B"
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 export const Navbar = () => {
     return (
         <nav>
-            <ul className="flex justify-between border-b border-black px-4">
+            <ul className="flex justify-between border-b border-black px-4 relative">
                 <li className="cursor-pointer">Экономика и общество</li>
                 <li className="cursor-pointer">Фондовый рынок</li>
                 <li className="cursor-pointer">Валютный рынок</li>
                 <li className="cursor-pointer">Другие рынки</li>
                 <li className="cursor-pointer">Финансовая инфраструктура</li>
                 <li className="cursor-pointer">Больше</li>
+                <div className="absolute top-0 left-0 w-full bg-white">
+                    <ul>
+                        <li>Экономика</li>
+                        <li>Экономика</li>
+                        <li>Экономика</li>
+                        <li>Экономика</li>
+                        <li>Экономика</li>
+                    </ul>
+                </div>
             </ul>
         </nav>
     );
